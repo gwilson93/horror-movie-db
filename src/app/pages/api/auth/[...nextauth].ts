f390a60dbc0505b9d133c97b733dfeffcd293787
Garret Wilson <gdwilso93@gmail.com>
2024-10-12 17:12:57 -0400

    Boilerplate setup for Google Authentication

---
+++ src/app/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@lib/mongodb";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  // Add custom pages if you want to customize the look of the auth pages
+  pages: {
+    signIn: "/auth/signin",
+    // signOut: '/auth/signout',
+    // error: '/auth/error',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token and user id from a provider.
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+
+      return session;
+    },
+  },
+});
